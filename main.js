7088f62195a442bea6871ae9f6c44431bdd268e1
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-04-29 23:11:06 -0400

    added gameController object

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const gameBoard = () => {
   ];
   const addMove = (x, y, player) => {
     if (gameArray[index] === "") {
-      board[x][y] = player;
+      board[x][y] = player.symbol;
     }
   };
   const getBoard = () => {
@@ -14,4 +14,26 @@ const gameBoard = () => {
   };
 
   return { addMove, getBoard };
+};
+
+const gameController = () => {
+  const players = [
+    {
+      id: "player1",
+      score: 0,
+      symbol: "X",
+    },
+    {
+      id: "player2",
+      score: 0,
+      symbol: "O",
+    },
+  ];
+
+  let currentPlayer = players[0];
+  let turns = 0;
+
+  const getPlayer = () => {
+    return players;
+  };
 };
